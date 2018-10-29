ce315405a2d3a845d4ca67509b903f4c0747d108
MrJulian01 <33683621+MrJulian01@users.noreply.github.com>
2018-10-29 15:29:11 +0100

    [ADDED] Home page

--- home.php
+++ home.php
@@ -1,38 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="author" content="parsa-gholamhossein">
-<meta name="description" content="danial-website"><!--Website name will be differend soon-->
-<meta name="keywords" content="HTML, CSS, PHP">
-<meta name="viewport" content="width=device-width, initial-scale=1,0">
-<title>danial website</title>
-<link href="css/styles.css" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"> 
-</head>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="J. Houwaart">
+    <meta name="description" content="danial-website"><!--Website name will be differend soon-->
+    <meta name="keywords" content="HTML, CSS, PHP">
+    <meta name="viewport" content="width=device-width, initial-scale=1,0">
+    <title>Mentaal Unique</title>
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+  </head>
+  <body>
 
-<body>
+    <!-- HEADER -->
+    <header>
+      <div>
+        <h1>Mentaal Unique</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,<br> sed do eiusmod tempor incididunt ut labore et.</p>
+        <button type="button" name="button">lorem ipsum</button>
+      </div>
+    </header>
 
-<section id="left">
-    <!--<img src="Images/tijdelijk-logo.png">HERE COMES THE LOGO-->
-    <p>Dit is een testbericht</p>
-</section>
+    <!-- NAVIGATION -->
+    <nav>
+      <article>
+        <a id="hamburger" href="#">&#9776;</a>
+      </article>
+      <article>
+        <!-- <a href="#home">Mentaal Unique</a> -->
+      </article>
+    </nav>
 
-<section id="right">
-    <!--nav-->
-    <a href="#">Home</a>
-    <a href="#">Over ons</a>
-    <a href="#">Contact</a>
-    <a href="#">Help</a>
-</section>
-<!--FIRST PART OF HOMEPAGE-->
-<section id="header">Donec id arcu pellentesque, pulvinar quam quis, laoreet mi. Donec enim arcu, auctor ac accumsan ac,<br> 
-porttitor ultricies sapien. Morbi et imperdiet nisl, sit amet dignissim magna. Donec efficitur libero mi,<br>
-at ultricies ex tristique non. In hac habitasse platea dictumst. Sed lobortis quis sem et pretium.<br>
-Sed sit amet odio iaculis, faucibus arcu in, rhoncus nibh. Nullam venenatis odio vitae lorem iaculis,<br> 
-quis scelerisque mi laoreet.</section>
-<!--SECOND PART OF WEBSITE-->
+    <main>
+      <section id="whatwedo">
+        <article>
+          <i class="fas fa-cogs"></i>
+          <h3>lorem ipsum</h3>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+        </article>
+        <article>
+          <i class="fas fa-cogs"></i>
+          <h3>lorem ipsum</h3>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+        </article>
+        <article>
+          <i class="fas fa-cogs"></i>
+          <h3>lorem ipsum</h3>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+        </article>
+      </section>
+    </main>
 
-[Eventueel meer tekst, het bovenste stukje kan ook in twee worden gesplit]
-</body>
+  </body>
 </html>
