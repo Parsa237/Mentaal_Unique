119cb196dc72d51499bb56e07ce67fb9eb0ad6d6
Parsa237 <32632126+Parsa237@users.noreply.github.com>
2018-10-30 13:24:33 +0100

    header.php map aangemaakt waarin de standaard header staat

---
+++ pages/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="J. Houwaart">
+    <meta name="description" content="danial-website"><!--Website name will be differend soon-->
+    <meta name="keywords" content="HTML, CSS, PHP">
+    <meta name="viewport" content="width=device-width, initial-scale=1,0">
+    <title>Mentaal Unique</title>
+    <link href="../scss/nav.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+  </head>
