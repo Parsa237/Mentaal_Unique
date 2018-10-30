b7026f7051a1ca92fce3f98d3024e07a730b823f
MrJulian01 <33683621+MrJulian01@users.noreply.github.com>
2018-10-30 16:12:20 +0100

    dit zijn wij pagina

--- pages/wiezijnwij.php
+++ pages/wiezijnwij.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="HTML, CSS, PHP">
     <meta name="viewport" content="width=device-width, initial-scale=1,0">
     <title>Mentaal Unique</title>
-    <link href="../scss/.css" rel="stylesheet" type="text/css">
+    <link href="../scss/wiezijnwij.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
   </head>
 
@@ -16,9 +16,7 @@
     <!-- HEADER -->
     <header class="header">
       <div class="div">
-        <h1 class="h1">Mentaal Unique</h1>
-        <p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit,<br> sed do eiusmod tempor incididunt ut labore et.</p>
-        <a class="button" href="#whatwedo">Wat wij doen</a>
+        <h1 class="h1">Wie zijn wij</h1>
       </div>
     </header>
 
@@ -33,6 +31,19 @@
         <a href="#info">Info</a>
       </div>
     </div>
+
+    <main>
+      <section class="top_section">
+        <h2>lorem ipsum</h2>
+        <article>
+          <img src="../img/groepsfoto.webp" alt="groepsfoto">
+        </article>
+        <article class="right">
+          <h3>Dit zij wij.</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </article>
+      </section>
+    </main>
 
   </body>
 </html>
