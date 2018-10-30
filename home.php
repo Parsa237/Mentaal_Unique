a0a806bad1169d51225beb09a69fe96d6e675c8c
MrJulian01 <33683621+MrJulian01@users.noreply.github.com>
2018-10-30 14:32:47 +0100

    nav bar klaar

--- home.php
+++ home.php
@@ -26,10 +26,10 @@
       <label for="toggle">&#9776;</label>
       <input type="checkbox" id="toggle"/>
       <div class="menu">
-        <a href="#">Home</a>
-        <a href="#">Wie zijn wij</a>
+        <a href="#home">Home</a>
+        <a href="#wiezijnwij">Wie zijn wij</a>
         <a href="contact.php">Contact</a>
-        <a href="#">Info</a>
+        <a href="#info">Info</a>
       </div>
     </div>
 
