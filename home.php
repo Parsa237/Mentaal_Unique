77c6244e38bf08c5bb50329d58206a132004bf07
MrJulian01 <33683621+MrJulian01@users.noreply.github.com>
2018-11-06 14:37:56 +0100

    [ADDED] Extra jQuery

--- home.php
+++ home.php
@@ -91,6 +91,25 @@
             });
           });
         });
+
+        // $(document).ready(function() {
+        //
+        //   var page_url = window.location.href;
+        //
+        //   var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
+        //
+        //   if (page_url == "whatwedo") {
+        //     $('html, body').animate({
+        //       scrollTop: $('#whatwedo' + page_id).offset().top + 20
+        //     }, 1000);
+        //   } else if (page_id == "whatwedo") {
+        //     $(html, body).animate({
+        //       scrollTop: $('#whatwedo' + page_id).offset().top
+        //     }, 1000);
+        //   }
+        //
+        // });
+
     </script>
 
   </body>
