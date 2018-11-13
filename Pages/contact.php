37e8028883bea72aad62f7e2e0a7c7638c3ef0de
Parsa237 <32632126+Parsa237@users.noreply.github.com>
2018-11-13 10:03:09 +0100

    [CHANGED] object aangemaakt met daarin info

--- Pages/contact.php
+++ Pages/contact.php
@@ -1,11 +1,35 @@
 <?php
 
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
 require_once("C:\\xampp\\htdocs\\Jouw eigen projecten\\Danial-website\\Website_klant\\APP\\includes\\PHPMailer-master\\src\\PHPMailer.php");
 require_once("C:\\xampp\\htdocs\\Jouw eigen projecten\\Danial-website\\Website_klant\\APP\\includes\\PHPMailer-master\\src\\Exception.php");
 require_once("C:\\xampp\\htdocs\\Jouw eigen projecten\\Danial-website\\Website_klant\\APP\\includes\\PHPMailer-master\\src\\SMTP.php");
 
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\PHPMailer;
+$MTo = "Parsadecoole@Gmail.com"; //This is were the email needs to be send to
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 1;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587; //Ports 25, 465 and 587 can be used
+$mail->IsHTML(true);
+$mail->Username = "Parsadecoole@Gmail.com";
+$mail->Password = "4H7p_Frubruy";
+$mail->SetFrom("gholamhosseinp929@gmail.com");
+$mail->Subject = $Vonderwerpen;
+$mail->Body = $Vbericht;
+$mail->AddAddress($MTo);
+$mail->Hostname = 'DESKTOP-DN89O2M';
+
+if(!$mail->send()){
+    echo "Email niet verstuurd";
+}else{
+    echo "Mail verstuurd";
+}
 
   class UserForm {
 //Making properties to later use
@@ -14,6 +38,7 @@ use PHPMailer\PHPMailer\PHPMailer;
     private $Vachternaam;
     private $Vemail;
     private $Vbericht;
+    private $Vonderwerpen;
     private $Bsubmit;
 
     public function __construct() {
@@ -24,6 +49,7 @@ use PHPMailer\PHPMailer\PHPMailer;
       $this->Vachternaam = $_POST["achternaam"];
       $this->Vemail = $_POST["email"];
       $this->Vbericht = $_POST["bericht"];
+      $this->Vonderwerpen = $_POST["onderwerpen"];
       $this->Bsubmit = $_POST["submit"];
     }
 
@@ -31,7 +57,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 //validating the inputs and email
 
-    if (!isset($this->Bsubmit) && empty($this->Vvoornaam) || empty($this->Vachternaam) || empty($this->Vemail) || empty($this->Vbericht)) {
+    if (!isset($this->Bsubmit) && empty($this->Vvoornaam) || empty($this->Vachternaam) || empty($this->Vemail) || empty($this->Vbericht) || empty($this->Vonderwerp)) {
       echo "U bent iets vergeten in te vullen";
       }else if(!filter_var($this->Vemail, FILTER_VALIDATE_EMAIL)) {
         echo "Uw email is onjuist";
@@ -64,7 +90,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 </head>
 <body>
 
-<!--TEMPORARY NAVIGATION JULIAN WILL CHANGE THIS IF NESSECARY-->
+<!--TEMPORARY NAVIGATION JULIAN WILL CHANGE THIS IF NESSESARY-->
 <!-- NAVIGATION -->
 <div class="nav">
       <label for="toggle">&#9776;</label>
@@ -80,22 +106,37 @@ use PHPMailer\PHPMailer\PHPMailer;
 
     <!--FORM, DESIGN WILL LATER BE IMPLEMENTED-->
     <!--DO NOT CHANGE THE NAME ID'S OR TYPES-->
+    <br>
     <form  method="post" action="contact.php">
     Voornaam:
     <br>
     <input type="text" name="voornaam" id="voornaam" placeholder="voornaam"><!--PLACEHOLDER CAN BE CHANGED-->
     <br>
+    <br>
     Achternaam:
     <br>
     <input type="text" name="achternaam" id="achternaam" placeholder="achternaam">
+    <br>
     <br>
     Uw Email:
     <br>
     <input type="text" name="email" id="email" placeholder="email">
     <br>
+    <br>
+    Onderwerp:
+    <br>
+    <select name="onderwerpen">
+        <option value="klacht">Klacht</option>
+        <option value="feedback">Feedback</option>
+        <option value="vraag">Vraag</option>
+        <option value="overig">Overig</option>
+    </select>
+    <br>
+    <br>
     Uw bericht:
     <br>
     <textarea name="bericht" id="bericht" cols="30" rows="10"></textarea>
+    <br>
     <br>
     <input type="submit" name="submit" id="submit" value="verzend!">
     </form>
