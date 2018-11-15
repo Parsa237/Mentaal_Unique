9fc7b4d1483a8757d09e0e5bd41b21b9fe80ae47
MrJulian01 <33683621+MrJulian01@users.noreply.github.com>
2018-11-15 13:45:53 +0100

    css gefixt

--- APP/pages/wiebenik.php
+++ APP/pages/wiebenik.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="HTML, CSS, PHP">
     <meta name="viewport" content="width=device-width, initial-scale=1,0">
     <title>Mentaal Unique</title>
-    <link href="../scss/wiezijnwij.min.css" rel="stylesheet" type="text/css">
+    <link href="../../scss/wiebenik.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
   </head>
 
@@ -36,7 +36,7 @@
       <section class="top_section">
         <h2>Lorem ipsum</h2>
         <article>
-          <img src="../img/groepsfoto.webp" alt="groepsfoto">
+          <img src="../../img/groepsfoto.webp" alt="groepsfoto">
         </article>
         <article class="right">
           <h3>Dit zijn wij.</h3>
@@ -50,7 +50,7 @@
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </article>
         <article>
-          <img src="../img/ouderen.jpg" alt="ouderen">
+          <img src="../../img/ouderen.jpg" alt="ouderen">
         </article>
       </section>
     </main>
