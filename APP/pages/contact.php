d72d04b5670a5f5389923a94f9b6f1a7f4236f83
Parsa237 <32632126+Parsa237@users.noreply.github.com>
2018-11-15 11:20:39 +0100

    [CHANGED] Pages map verwijderd en Overig map

--- APP/pages/contact.php
+++ APP/pages/contact.php
@@ -1,54 +1,5 @@
-<?php
-
-require_once("C:\\xampp\\htdocs\\Jouw eigen projecten\\Danial-website\\Website_klant\\APP\\includes\\PHPMailer-master\\src\\PHPMailer.php");
-require_once("C:\\xampp\\htdocs\\Jouw eigen projecten\\Danial-website\\Website_klant\\APP\\includes\\PHPMailer-master\\src\\Exception.php");
-require_once("C:\\xampp\\htdocs\\Jouw eigen projecten\\Danial-website\\Website_klant\\APP\\includes\\PHPMailer-master\\src\\SMTP.php");
-
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\PHPMailer;
-
-  class UserForm {
-//Making properties to later use
-
-    private $Vvoornaam;
-    private $Vachternaam;
-    private $Vemail;
-    private $Vbericht;
-    private $Bsubmit;
-
-    public function __construct() {
-
-//Giving the properties a value
-
-      $this->Vvoornaam = $_POST["voornaam"];
-      $this->Vachternaam = $_POST["achternaam"];
-      $this->Vemail = $_POST["email"];
-      $this->Vbericht = $_POST["bericht"];
-      $this->Bsubmit = $_POST["submit"];
-    }
-
-    public function Index() {
-
-//validating the inputs and email
-
-    if (!isset($this->Bsubmit) && empty($this->Vvoornaam) || empty($this->Vachternaam) || empty($this->Vemail) || empty($this->Vbericht)) {
-      echo "U bent iets vergeten in te vullen";
-      }else if(!filter_var($this->Vemail, FILTER_VALIDATE_EMAIL)) {
-        echo "Uw email is onjuist";
-      }else{
-        
-        $Vvoornaam = $_POST["voornaam"];
-        $Vachternaam = $_POST["achternaam"];
-        $Vemail = $_POST["email"];
-
-      }
-    }
-  }
-  
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') { 
-    $userForm = new UserForm(); 
-    $userForm->Index(); 
-  }
+<?php 
+require_once("C:\\xampp\\htdocs\\Jouw eigen projecten\\Danial-website\\Website_klant\\APP\\includes\\contact-form.php"); 
 ?>
 <html lang="en" dir="ltr">
 <head>
@@ -64,13 +15,13 @@ use PHPMailer\PHPMailer\PHPMailer;
 </head>
 <body>
 
-<!--TEMPORARY NAVIGATION JULIAN WILL CHANGE THIS IF NESSECARY-->
+<!--TEMPORARY NAVIGATION JULIAN WILL CHANGE THIS IF NESSESARY-->
 <!-- NAVIGATION -->
 <div class="nav">
       <label for="toggle">&#9776;</label>
       <input type="checkbox" id="toggle"/>
       <div class="menu">
-        <a href="home.html">Home</a>
+        <a href="home.php">Home</a>
         <a href="wiezijnwij.php">Wie zijn wij</a>
         <a href="pages/contact.php">Contact</a>
         <a href="#info">Info</a>
@@ -80,22 +31,33 @@ use PHPMailer\PHPMailer\PHPMailer;
 
     <!--FORM, DESIGN WILL LATER BE IMPLEMENTED-->
     <!--DO NOT CHANGE THE NAME ID'S OR TYPES-->
+    <br>
     <form  method="post" action="contact.php">
     Voornaam:
     <br>
-    <input type="text" name="voornaam" id="voornaam" placeholder="voornaam"><!--PLACEHOLDER CAN BE CHANGED-->
+    <input type="text" name="voornaam" id="voornaam" placeholder="voornaam" autocomplete="off"><!--PLACEHOLDER CAN BE CHANGED-->
     <br>
     Achternaam:
     <br>
-    <input type="text" name="achternaam" id="achternaam" placeholder="achternaam">
+    <input type="text" name="achternaam" id="achternaam" placeholder="achternaam" autocomplete="off"><!--autocomplete prevents examples being given-->
     <br>
     Uw Email:
     <br>
-    <input type="text" name="email" id="email" placeholder="email">
+    <input type="text" name="email" id="email" placeholder="email" autocomplete="off">
+    <br>
+    Onderwerp:
+    <br>
+    <select name="onderwerpen">
+        <option value="klacht">Klacht</option>
+        <option value="feedback">Feedback</option>
+        <option value="vraag">Vraag</option>
+        <option value="overig">Overig</option>
+    </select>
     <br>
     Uw bericht:
     <br>
     <textarea name="bericht" id="bericht" cols="30" rows="10"></textarea>
+    <br>
     <br>
     <input type="submit" name="submit" id="submit" value="verzend!">
     </form>
