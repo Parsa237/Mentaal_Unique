7005619365f02fe25eaf124e0f434516c494abbb
liammeijles <liammeijles@gmail.com>
2018-11-17 19:23:41 +0100

    [ADDED] banner slideshow + W.I.P. transition effect

--- js/slides.js
+++ js/slides.js
@@ -1,4 +1,21 @@
-function bannerSlider() {
-    document.getElementsByClassName('header').style.backgroundImage = 'url("img/zorg.jpg")';
-    alert ("hoi");
-}
+$(function() {
+    var header = $('.header');
+    var backgrounds = new Array(
+    'url(../../img/zorg.jpg)',
+    'url(../../img/zorg2.jpg)',
+    'url(../../img/zorg3.jpg)',
+    'url(../../img/zorg4.jpg)'
+    );
+    var current = 0;
+
+    function nextBackground() {
+        header.css(
+        'background-image',
+        backgrounds[current = ++current % backgrounds.length]
+        );
+        
+        setTimeout(nextBackground, 7000);
+        }
+        setTimeout(nextBackground, 7000);
+        header.css('background-image', backgrounds[0]);
+        });
