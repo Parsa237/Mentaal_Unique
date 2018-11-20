08f51d48a6ec08fa44155b366cfef40c0b354cfe
Parsa237 <32632126+Parsa237@users.noreply.github.com>
2018-11-20 10:24:12 +0100

    [CHANGED] else toegevoegd en variabele naam veranderd

--- APP/includes/contact-form.php
+++ APP/includes/contact-form.php
@@ -1,5 +1,46 @@
 <?php
 
+class UserForm {
+
+//Making properties to later use
+    private $Vvoornaam;
+    private $Vachternaam;
+    private $Vemail;
+    private $Vbericht;
+    private $Vonderwerp;
+    private $Bsubmit;
+
+    public function __construct() {
+    }
+
+    public function ValidateValues() {
+
+    //Giving the properties a value
+    $this->Vvoornaam = $_POST["voornaam"];
+    $this->Vachternaam = $_POST["achternaam"];
+    $this->Vemail = $_POST["email"];
+    $this->Vbericht = $_POST["bericht"];
+    $this->Vonderwerp = $_POST["onderwerp"];
+    $this->Bsubmit = $_POST["submit"];
+
+
+    //validating the inputs and email
+    if (!isset($this->Bsubmit) && empty($this->Vvoornaam) || empty($this->Vachternaam) || empty($this->Vemail) || empty($this->Vbericht) || empty($this->Vonderwerp)) {
+      echo "U bent iets vergeten in te vullen";
+      } else if(!filter_var($this->Vemail, FILTER_VALIDATE_EMAIL)) {
+        echo "Uw email is onjuist";
+      } else {
+        echo "Mail verstuurd";
+    }
+    }
+  }
+  
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') { 
+    $userForm = new UserForm(); 
+    $userForm->ValidateValues(); 
+  }
+
+  
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 
@@ -19,52 +60,9 @@ $mail->IsHTML(true);
 $mail->Username = "Parsadecoole@Gmail.com";
 $mail->Password = "4H7p_Frubruy";
 $mail->SetFrom("Parsadecoole@Gmail.com");//Waar de email vanaf wordt gestuurd
-$mail->Subject = "Voorlopig";
-$mail->Body = "Dit is voorlopig";
+$mail->Subject = $_POST["onderwerp"];
+$mail->Body = $_POST["bericht"];
 $mail->AddAddress($MTo);
 $mail->Hostname = 'DESKTOP-DN89O2M';
 
-if(!$mail->send()) {
-    echo "Email niet verstuurd";
-} else {
-    echo "Mail verstuurd";
-}
-
-class UserForm {
-
-//Making properties to later use
-    private $Vvoornaam;
-    private $Vachternaam;
-    private $Vemail;
-    private $Vbericht;
-    private $Vonderwerpen;
-    private $Bsubmit;
-
-    public function __construct() {
-    }
-
-    public function ValidateValues() {
-
-    //Giving the properties a value
-    $this->Vvoornaam = $_POST["voornaam"];
-    $this->Vachternaam = $_POST["achternaam"];
-    $this->Vemail = $_POST["email"];
-    $this->Vbericht = $_POST["bericht"];
-    $this->Vonderwerpen = $_POST["onderwerpen"];
-    $this->Bsubmit = $_POST["submit"];
-
-
-    //validating the inputs and email
-    if (!isset($this->Bsubmit) && empty($this->Vvoornaam) || empty($this->Vachternaam) || empty($this->Vemail) || empty($this->Vbericht) || empty($this->Vonderwerp)) {
-      echo "U bent iets vergeten in te vullen";
-      } else if(!filter_var($this->Vemail, FILTER_VALIDATE_EMAIL)) {
-        echo "Uw email is onjuist";
-      }
-    }
-  }
-  
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') { 
-    $userForm = new UserForm(); 
-    $userForm->ValidateValues(); 
-  }
 ?>
